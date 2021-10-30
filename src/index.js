3a5e941ead77957da853481f97961ed53a03a16e
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2021-10-30 16:43:29 +0500

    Project is completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import moment from "moment";
 import cubejs from "@cubejs-client/core";
 import Chart from "chart.js/auto";
-import moment from "moment";
+import flatpickr from "flatpickr";
+import "./styles.css";
 
 const cubejsApi = cubejs(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTQ2NjY4OTR9.0fdi5cuDZ2t3OSrPOMoc3B1_pwhnWj4ZmM3FHEX7Aus",
@@ -8,23 +10,60 @@ const cubejsApi = cubejs(
         apiUrl:"https://awesome-ecom.gcp-us-central1.cubecloudapp.dev/cubejs-api/v1",
     }
 );
+let chart;
 
-cubejsApi
-    .load({
-        measures: ["Orders.count"],
-        timeDimensions: [
+var drawChart = function (startDate, endDate) {
+    cubejsApi
+        .load({
+            measures: ["Orders.count"],
+            timeDimensions: [
+                {
+                    dimension: "Orders.createdAt",
+                    granularity: `day`,
+                    dateRange: [startDate, endDate],
+                },
+            ],
+        })
+        .then((resultSet) => {
+            if (chart) {
+                chart.data = chartJsData(resultSet);
+                chart.update();
+            } 
+            else {
+                chart = new Chart(document.getElementById("chart"), {type: "line",options: {},data: chartJsData(resultSet),});
+            }
+        });
+};
+const MIN_DATE = "2020-08-01";
+const MAX_DATE = "2020-09-01";
+
+flatpickr("#dates", {
+    mode: "range",
+    dateFormat: "Y-m-d",
+    defaultDate: [MIN_DATE, MAX_DATE],
+
+    onChange: function (selectedDates) {
+        if (selectedDates.length === 2) {
+            drawChart(selectedDates[0], selectedDates[1]);
+        }
+    },
+});
+
+drawChart(MIN_DATE, MAX_DATE);
+
+var chartJsData = function (resultSet) {
+    return {
+        datasets: [
             {
-                dimension: "Orders.createdAt",
-                granularity: `day`,
-                dateRange: [`08/01/2020`, `09/01/2020`],
+                label: "Orders Count",
+                data: resultSet.chartPivot().map(function (r) {
+                    return r["Orders.count"];
+                }),
+                backgroundColor: "rgb(255, 99, 132)",
             },
         ],
-    })
-    .then((resultSet) => {
-        new Chart(document.getElementById("chart"), {
-            type: "line",
-            options: {},
-            data: chartJsData(resultSet),
-        });
-    });
-
+        labels: resultSet.categories().map(function (c) {
+            return moment(c.x).format("DD MMM");
+            }),
+    };
+};
