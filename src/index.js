7acc11d37cb03500f392276c03b7ebeb78f6fac8
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2021-10-30 20:11:45 +0500

    Docker runs

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./styles.css";
 const cubejsApi = cubejs(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTQ2NjY4OTR9.0fdi5cuDZ2t3OSrPOMoc3B1_pwhnWj4ZmM3FHEX7Aus",
     {
-        apiUrl:"https://awesome-ecom.gcp-us-central1.cubecloudapp.dev/cubejs-api/v1",
+        apiUrl:"https://localhost:4000/cubejs-api/v1",
     }
 );
 let chart;
@@ -50,6 +50,25 @@ flatpickr("#dates", {
 });
 
 drawChart(MIN_DATE, MAX_DATE);
+
+// let static_chart = cubejsApi
+//   .load({
+//     measures: ["Orders.count"],
+//     timeDimensions: [
+//       {
+//         dimension: "Orders.createdAt",
+//         granularity: `day`,
+//         dateRange: [`08/01/2020`, `09/01/2020`],
+//       },
+//     ],
+//   })
+//   .then((resultSet) => {
+//     new Chart(document.getElementById("chart"), {
+//       type: "line",
+//       options: {},
+//       data: chartJsData(resultSet),
+//     });
+//   });
 
 var chartJsData = function (resultSet) {
     return {
